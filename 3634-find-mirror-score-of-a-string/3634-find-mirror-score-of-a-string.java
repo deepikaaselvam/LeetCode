06566002329fa3fb07a1ce85ccd5cfb941da7168
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-01-05 11:26:31 +0530

    Time: 32 ms (100%), Space: 45.5 MB (100%) - LeetHub

---
+++ 3634-find-mirror-score-of-a-string/3634-find-mirror-score-of-a-string.java
@@ -0,0 +1,45 @@
+class Solution {
+    public long calculateScore(String s) {
+        int n = s.length();
+        long totalScore = 0;
+        Map<Character, LinkedList<Integer>> charMap = new HashMap<>();
+        for (int i = 0; i < n; i++) 
+        {
+            char currentChar = s.charAt(i);
+            char mirrorChar = (char) ('z' - (currentChar - 'a')); 
+            if (charMap.containsKey(mirrorChar) && !charMap.get(mirrorChar).isEmpty()) 
+            {
+                int j = charMap.get(mirrorChar).removeLast(); 
+                totalScore += (i - j); 
+            } 
+            else {
+                charMap.putIfAbsent(currentChar, new LinkedList<>());
+                charMap.get(currentChar).add(i);
+            }
+        }
+
+        return totalScore;
+        
+                  
+    }
+}
+        /*int n = s.length();
+        long totalScore = 0;
+        Map<Character, LinkedList<Integer>> charMap = new HashMap<>();
+        for (int i = 0; i < n; i++) 
+        {
+            char currentChar = s.charAt(i);
+            char mirrorChar = (char) ('z' - (currentChar - 'a')); 
+            if (charMap.containsKey(mirrorChar) && !charMap.get(mirrorChar).isEmpty()) 
+            {
+                int j = charMap.get(mirrorChar).removeLast(); 
+                totalScore += (i - j); 
+            } 
+            else {
+                charMap.putIfAbsent(currentChar, new LinkedList<>());
+                charMap.get(currentChar).add(i);
+            }
+        }
+
+        return totalScore;*/
+  
