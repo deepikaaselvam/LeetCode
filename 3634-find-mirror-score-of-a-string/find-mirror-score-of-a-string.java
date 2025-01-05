1478808d02ba18d849e1970dc2480e65dd04c45b
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-01-05 11:26:34 +0530

    Time: 32 ms (100.00%) | Memory: 45.5 MB (100.00%) - LeetSync

--- 3634-find-mirror-score-of-a-string/find-mirror-score-of-a-string.java
+++ 3634-find-mirror-score-of-a-string/find-mirror-score-of-a-string.java
@@ -20,5 +20,26 @@ class Solution {
 
         return totalScore;
         
+                  
     }
 }
+        /*int n = s.length();
+        long totalScore = 0;
+        Map<Character, LinkedList<Integer>> charMap = new HashMap<>();
+        for (int i = 0; i < n; i++) 
+        {
+            char currentChar = s.charAt(i);
+            char mirrorChar = (char) ('z' - (currentChar - 'a')); 
+            if (charMap.containsKey(mirrorChar) && !charMap.get(mirrorChar).isEmpty()) 
+            {
+                int j = charMap.get(mirrorChar).removeLast(); 
+                totalScore += (i - j); 
+            } 
+            else {
+                charMap.putIfAbsent(currentChar, new LinkedList<>());
+                charMap.get(currentChar).add(i);
+            }
+        }
+
+        return totalScore;*/
+  
