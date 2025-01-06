f20730259701812efbe14c3cfaf1a527a37b9f40
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-01-06 19:51:45 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> hash=new HashMap<>();
+        HashMap<Integer,Integer> mpp=new HashMap<>();
         for(int i=0;i<nums.length;i++)
         {
             Integer diff=(target-nums[i]);
-            if(hash.containsKey(diff))
+            if(mpp.containsKey(diff))
             {
-                int ret[]={hash.get(diff),i};
+                int ret[]={mpp.get(diff),i};
                 return ret;
             }
-            hash.put(nums[i],i);
+            mpp.put(nums[i],i);
         }
         return null;
         
