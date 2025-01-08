a204c4a7b7cb4fffe80479ed4e6c2941bee1d758
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-01-08 10:09:02 +0530

    Time: 6 ms (20.93%), Space: 43.7 MB (5.12%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
@@ -1,5 +1,93 @@
 class Solution {
-    public List<String> stringMatching(String[] words) {
+    public List<String> stringMatching(String[] words) 
+    {
+        //KMP Algorithm
+        ArrayList<String> result= new ArrayList<>();
+        Arrays.sort(words,(a,b)->a.length()-b.length());
+        for(int i=0;i<words.length;i++)
+        {
+            for(int j = i+1;j<words.length;j++)
+            {
+                if(isPatmatch(words[j],words[i]))
+                {
+                    result.add(words[i]);
+                    break;
+                }
+
+            }
+        }
+        return result;
+    }
+    public static int[]constructLPSArray(String pattern,int[]lps)
+    {
+        int len =0;
+        lps[0]=0;
+        int i=1;
+        while(i<pattern.length())
+        {
+            if(pattern.charAt(i)==pattern.charAt(len))
+            {
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else
+            {
+                //mismatch
+                if(len!=0)
+                {
+                    len=lps[len-1];
+
+                }
+                else
+                {
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+    public boolean isPatmatch(String text,String pattern)
+    {
+        if(pattern.length()==0)
+        {
+            return false;
+        }
+        if(text.length()<pattern.length())
+        {
+            return false;
+        }
+        int[]lpsArray= new int[pattern.length()];
+        int[]lps=constructLPSArray(pattern,lpsArray);
+        int i=0;int j=0;
+        while(i<text.length())
+        {
+            if(text.charAt(i)==pattern.charAt(j))
+            {
+                i++;
+                j++;
+                if(j==pattern.length())
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                if(j!=0)
+                {
+                    j=lps[j-1];
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+        return false;
+    }   
+}
+        /* Nested Loop
         ArrayList<String>result = new ArrayList<>();
         for(int i=0;i<words.length;i++)
         {
@@ -12,39 +100,6 @@ class Solution {
                 }
             }
         }
-        return result;
-        // public static String[] sort(String []s)
-        // {
-        //     for (int i=1 ;i<s.length; i++)
-        //     {
-        //         String temp = s[i];
+        return result;*/
         
-        //         // Insert s[j] at its correct position
-        //         int j = i - 1;
-        //         while (j >= 0 && temp.length() < s[j].length())
-        //         {
-        //             s[j+1] = s[j];
-        //             j--;
-        //         }
-        //         s[j+1] = temp;
-        //     }
-        // }
-        // String[]ans = sort(words);
-        // String newString[] =new String[ans.length()];
-        // ƒor(int i=0;i<ans.length;i++)
-        // {
-        //     newString[i]=ans[i];
-        // }
-        // for(int i=0;i<newString.length-1;i++)
-        // {
-        //     String currentWord=newString[i];
-        //     for(int j=i+1;j<newString.length;j++)
-        //     {
-        //         if()
-        //     }
-
-        // }
-       
         
-    }
-}
