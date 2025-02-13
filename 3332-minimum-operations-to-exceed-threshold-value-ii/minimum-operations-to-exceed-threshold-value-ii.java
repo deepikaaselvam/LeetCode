4becab9a7f5365710b0b84c82542b702b52c7296
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-02-13 21:30:11 +0530

    Time: 152 ms (37.45%) | Memory: 70.7 MB (60.91%) - LeetSync

---
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+  
+    public int minOperations(int[] nums, int k) {
+        PriorityQueue<Long>minHeap = new PriorityQueue<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            minHeap.add((long)nums[i]);
+        }
+        int numOfOperations =0;
+        while(minHeap.peek()<k)
+        {
+            long x = minHeap.remove();
+            long y = minHeap.remove();
+            minHeap.add((long)Math.min(x,y)*2 + (long)Math.max(x,y));
+
+            numOfOperations++;
+        }
+        return numOfOperations;
+       
+
+        
+    }
+}
+  // public static ArrayList<Integer> getTwoSmallestElement(ArrayList<Integer>nums1)
+    // {
+    //     // int small=Integer.MAX_VALUE;
+    //     // int sec_small= Integer.MAX_VALUE;
+    //     // for(int i=0;i<nums1.size();i++)
+    //     // {
+    //     //     if(nums1.get(i)<small)
+    //     //     {
+    //     //         sec_small=small;
+    //     //         small=nums1.get(i);
+    //     //     }
+    //     //     else if(nums1.get(i)>small && nums1.get(i)<sec_small)
+    //     //     {
+    //     //         sec_small = nums1.get(i);
+    //     //     }
+    //     // }
+    // }
