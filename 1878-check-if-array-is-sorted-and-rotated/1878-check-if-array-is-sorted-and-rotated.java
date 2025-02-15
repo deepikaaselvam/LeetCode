bb9585ee40ee11ff3ea4c8a02ed51b0aa0e9b299
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-02-15 10:10:35 +0530

    Time: 0 ms (100%), Space: 41.7 MB (11.86%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -5,7 +5,9 @@ class Solution {
         int cnt=0;
         for(int i=0;i<n;i++)
         {
-            if(nums[i]>nums[(i+1)%nums.length])//Logic of is to be sorted  
+            if(nums[i]>nums[(i+1)%nums.length])//Logic of is to be sorted ..
+                                            //Mainly "Rotated"->. To find the cnt of "CurrentNo">NextNo  ..if suppose the cnt is  =1..is rotated and sorted 
+                                // if suppose the cnt >1 it will return false
             {
                 cnt++;
             }
