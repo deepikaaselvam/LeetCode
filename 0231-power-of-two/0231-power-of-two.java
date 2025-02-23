0682c74c85a5c7cc935e13f1ed2bf5917e5d8e44
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-02-24 01:21:29 +0530

    Time: 1 ms (42.13%), Space: 40.7 MB (90.02%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,31 +1,14 @@
 class Solution {
    public static boolean isPowerofTwoo(int qn)
     {
-        // int ans=qn%2;
-        if(qn==1)
-        {
-            return true;
-        }
-        if(qn%2!=0)
-        {
-            return false;
-        }
-        
-        
-        
+        if(qn==1) return true;
+        if(qn%2!=0) return false;
         return isPowerofTwoo(qn/2);
      }
     
     public  boolean isPowerOfTwo(int n) {
-        // int value =n;
-        if(n<1)
-        {
-            return false;
-        }
-        if(n==1)
-        {
-            return true;
-        }
+       if(n<1) return false;
+       if(n==1) return true;
         boolean ans= isPowerofTwoo(n);
         return ans;
 
