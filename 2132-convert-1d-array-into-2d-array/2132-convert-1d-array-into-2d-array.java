ae38f7cf7da6f332a335d9256038fb312ba44270
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-03-02 23:45:42 +0530

    Time: 4 ms (56.43%), Space: 55.6 MB (69.97%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
@@ -5,14 +5,14 @@ class Solution {
         {
             return new int[][]{};
         }
-        int OriginalArrayIndex=0;
+        // int OriginalArrayIndex=0;
         int row,col;
         for(row=0;row<m;row+=1)
         {
             for(col=0;col<n;col+=1)
             {
-                TwoDArray[row][col]=original[OriginalArrayIndex];
-                OriginalArrayIndex+=1;
+                TwoDArray[row][col]=original[row*n+col];
+                // OriginalArrayIndex+=1;
             }
         }
         return TwoDArray;
