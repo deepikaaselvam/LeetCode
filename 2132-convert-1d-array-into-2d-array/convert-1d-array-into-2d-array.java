3aff828483a935f37b545016397a60b4a62e4051
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-03-02 23:43:51 +0530

    Time: 6 ms (7.75%) | Memory: 56 MB (9.72%) - LeetSync

---
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int[][]TwoDArray = new int[m][n];
+        if(original.length !=(m*n))
+        {
+            return new int[][]{};
+        }
+        int OriginalArrayIndex=0;
+        int row,col;
+        for(row=0;row<m;row+=1)
+        {
+            for(col=0;col<n;col+=1)
+            {
+                TwoDArray[row][col]=original[OriginalArrayIndex];
+                OriginalArrayIndex+=1;
+            }
+        }
+        return TwoDArray;
+    }
+}
