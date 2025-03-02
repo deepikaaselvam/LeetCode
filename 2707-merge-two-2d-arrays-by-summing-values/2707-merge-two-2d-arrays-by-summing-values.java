dafcca6009a17f7d772da966cb476d03a364e66f
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-03-03 01:08:57 +0530

    Time: 1 ms (87.11%), Space: 45.1 MB (78.71%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
@@ -1,6 +1,52 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        TreeMap<Integer,Integer>twoDArray = new TreeMap<>();
+        ArrayList<int[]>IntegerArray = new ArrayList<>();
+        int ptr1 =0;
+        int ptr2 =0;
+        while(ptr1<nums1.length && ptr2 <nums2.length)
+        {
+            int[]eachFieldArray = new int[2];
+            if(nums1[ptr1][0]==nums2[ptr2][0])
+            {
+                eachFieldArray[0]=nums1[ptr1][0];
+                eachFieldArray[1]=nums1[ptr1][1]+nums2[ptr2][1];
+                ptr1+=1;
+                ptr2+=1;
+            }
+            else if(nums1[ptr1][0]<nums2[ptr2][0])
+            {
+                eachFieldArray[0]=nums1[ptr1][0];
+                eachFieldArray[1]=nums1[ptr1][1];
+                ptr1+=1;
+            }
+            else
+            {
+                eachFieldArray[0]=nums2[ptr2][0];
+                eachFieldArray[1]=nums2[ptr2][1];
+                ptr2+=1;
+            }
+            IntegerArray.add(eachFieldArray);
+        }
+        while(ptr1<nums1.length)
+        {
+
+              IntegerArray.add(nums1[ptr1]); 
+                ptr1+=1;
+        }
+        while(ptr2<nums2.length)
+        {
+            IntegerArray.add(nums2[ptr2]);
+            ptr2+=1;
+        }
+        return IntegerArray.toArray(new int[IntegerArray.size()][]);
+         
+       
+
+        
+    }
+}
+/*
+ TreeMap<Integer,Integer>twoDArray = new TreeMap<>();
         for(int[] nums:nums1)
         {
             twoDArray.put(nums[0],nums[1]);
@@ -27,7 +73,4 @@ class Solution {
             mergeTwoDArray[i]=mergeList.get(i);
         }
         return mergeTwoDArray;
-
-        
-    }
-}
+*/
