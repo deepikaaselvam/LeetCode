85db6e77c4a5bea4d2259e8dfa3092d93d5f98fa
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-03-03 21:47:17 +0530

    Time: 4 ms (100%), Space: 68.3 MB (24.11%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -1,6 +1,33 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-         //less than Pivot ->List1
+        int[]answer = new int[nums.length];
+        int lesser=0;
+        int greater = nums.length-1;
+        for(int ind1 =0,ind2=nums.length-1;ind1<nums.length;ind1++,ind2--)
+        {
+            if(nums[ind1]<pivot)
+            {
+                answer[lesser]=nums[ind1];
+                lesser+=1;
+            }
+             if(nums[ind2]>pivot)
+            {
+                answer[greater]=nums[ind2];
+                greater--;
+            }
+        }
+        while(lesser<=greater)
+        {
+            answer[lesser]=pivot;
+            lesser+=1;
+        }
+        return answer;
+
+        
+    }
+}
+/*
+ //less than Pivot ->List1
         ArrayList<Integer>list1 = new ArrayList<>();
         int cnt=0;
         //greater than pivot ->List2
@@ -39,5 +66,5 @@ class Solution {
                 ind2++;
             }
         return answer;
-    }
-}
+
+        */
