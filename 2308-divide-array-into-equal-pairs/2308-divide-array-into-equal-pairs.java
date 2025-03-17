465fe62493f126f55426e5ba034bbbbc36b07f5d
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-03-17 23:43:36 +0530

    Time: 1 ms (100%), Space: 44.5 MB (58.82%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -1,20 +1,35 @@
 class Solution {
     public boolean divideArray(int[] nums) {
-        //HashMap
-        HashMap<Integer,Integer>mpp = new HashMap<>();
+        //Array 
+        int[]frequency = new int[501];
         for(int index =0;index<nums.length;index+=1)
         {
-            mpp.put(nums[index],mpp.getOrDefault(nums[index],0)+1);
+            frequency[nums[index]]+=1;
         }
-        for(int cnt:mpp.values())
+        for(int i =0;i<frequency.length;i+=1)
         {
-            if(cnt%2!=0)
+            if(frequency[i]%2!=0)
             {
                 return false;
             }
         }
         return true;
     }
+    //     //HashMap
+    //     HashMap<Integer,Integer>mpp = new HashMap<>();
+    //     for(int index =0;index<nums.length;index+=1)
+    //     {
+    //         mpp.put(nums[index],mpp.getOrDefault(nums[index],0)+1);
+    //     }
+    //     for(int cnt:mpp.values())
+    //     {
+    //         if(cnt%2!=0)
+    //         {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
     //   //xor gate;
     //   int xor =0;
     //   for(int index =0;index<nums.length ;index+=1)
