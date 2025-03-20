ad9f5344217c89d3d4488d3f12161d06fd627d72
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-03-20 20:18:22 +0530

    Time: 26 ms (26.53%) | Memory: 45.1 MB (61.86%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.java
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -9,7 +9,7 @@ class Solution {
             answer[nums[index]]+=1;}
         }
         int maxi =0;
-        int value =0;int cnt =0;
+        
         int safe =0;boolean flag =false;
         for(int index =0;index<answer.length;index+=1)
         {
@@ -17,7 +17,7 @@ class Solution {
             {
                 maxi=answer[index];
                 safe =index;
-                value =answer[index];
+         
                 flag =true;
 
             }
