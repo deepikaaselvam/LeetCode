43532b4d3a48d3e4bd28f5ec371b8e1ad776c0e1
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-03-20 20:18:20 +0530

    Time: 26 ms (26.53%), Space: 45.1 MB (61.86%) - LeetHub

---
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int mostFrequentEven(int[] nums) {
+        
+    int[]answer = new int[100000];
+        for(int index =0;index<nums.length;index+=1)
+        {
+            if(nums[index]%2==0)
+            {
+            answer[nums[index]]+=1;}
+        }
+        int maxi =0;
+        
+        int safe =0;boolean flag =false;
+        for(int index =0;index<answer.length;index+=1)
+        {
+            if(answer[index]>maxi)
+            {
+                maxi=answer[index];
+                safe =index;
+         
+                flag =true;
+
+            }
+           
+        }
+            
+      if(flag==false){return -1;}//no even number}
+     return safe;
+    }
+}
