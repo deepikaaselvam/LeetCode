2e40f024cb7448a9fb75e9dcb4d6df084f17e77b
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-03-26 09:43:51 +0530

    Time: 1 ms (82.94%), Space: 42.1 MB (30.34%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder answer = new StringBuilder();
+            int Length1 = word1.length();
+            int Length2 = word2.length();
+            int total =Length1+Length2;
+            int cnt1 =0;
+            int cnt2=0;
+          
+            for(int index =0;index<(total);index+=1)
+            {
+            
+                if(cnt1==word1.length())
+                {
+                    answer.append(word2.charAt(cnt2));
+                    cnt2+=1;
+                    // continue;
+                }
+                else if(cnt2==word2.length())
+                {
+                    answer.append(word1.charAt(cnt1));
+                    cnt1+=1;
+                    // continue;
+                }
+                else if(index%2 ==0)
+                {
+                    answer.append(word1.charAt(cnt1));
+                    cnt1+=1;
+                }
+                else{
+                    answer.append(word2.charAt(cnt2));
+                    cnt2+=1;
+
+                }
+            }
+            return answer.toString();
+        
+    }
+}
