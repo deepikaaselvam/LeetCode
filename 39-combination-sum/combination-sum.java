1b80d37b46c03cd955acecafbc679da04c3209d8
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-04-07 10:18:20 +0530

    Time: 1 ms (99.90%) | Memory: 44.3 MB (96.56%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,28 +1,23 @@
 class Solution {
-      private static void combinationSumI(int index,int []nums,int target,ArrayList<Integer>list,List<List<Integer>>ans)
+    public static void combinationSumI(int i,int[]array,int  target,              ArrayList<Integer>list,List<List<Integer>>answer)
     {
-      
-        if(index>=nums.length)
+        if(i==array.length)
         {
-            if(target ==0)
+            if(target==0)
             {
-                ans.add(new ArrayList<>(list));
+                answer.add(new ArrayList<>(list));
             }
             return;
         }
-        if(nums[index]<=target)
+        if(array[i]<=target)
         {
-            list.add(nums[index]);
-            target = target - nums[index];
-            combinationSumI(index,nums,target,list,ans);
-            target +=list.get(list.size()-1);
+            list.add(array[i]);
+            combinationSumI(i,array,target-array[i],list,answer);
             list.remove(list.size()-1);
         }
-        combinationSumI(index+1,nums,target,list,ans);
-    }
-  
+        combinationSumI(i+1,array,target,list,answer);
         
-    
+    }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
        List<List<Integer>>ans=new ArrayList<>();
        ArrayList<Integer>list=new ArrayList<>();
