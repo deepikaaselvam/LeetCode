93232d03b9c63f532aff127d7ed120d1d919edb0
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-04-30 14:03:04 +0530

    Time: 4 ms (23.82%), Space: 43.4 MB (14.12%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.java
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.java
@@ -1,8 +1,31 @@
 class Solution {
+    private int getDivisiblePairs(List<Integer>previousIndex,int currentIndex,int k)
+    {
+        int count=0;
+        for(int previous:previousIndex)
+        {
+            count+=((previous*currentIndex)%k==0)?1:0;
+        }
+        return count;
+    }
+
     public int countPairs(int[] nums, int k) {
         int count =0;
-        // HashMap<Integer,Integer>mpp = new HashMap<>();//index,nums[index]
-        for(int index =0;index<nums.length-1;index++)
+        HashMap<Integer,List<Integer>>mpp = new HashMap<>();//index,nums[index]
+        for(int ind =0;ind<nums.length;ind+=1)
+        {
+            if(mpp.containsKey(nums[ind]))
+            {
+                count+=getDivisiblePairs(mpp.get(nums[ind]),ind,k);
+            }
+            mpp.computeIfAbsent(nums[ind],x->new ArrayList<>()).add(ind);
+        }
+        return count;
+       
+    }
+}
+/*
+ for(int index =0;index<nums.length-1;index++)
         {
            for(int jindex=index+1;jindex<nums.length;jindex+=1)
            {
@@ -18,8 +41,7 @@ class Solution {
         }
         return count;
         
-    }
-}
+        */
 /*
  if(mpp.containsKey(nums[index]))
             {
