bab1f829fe3530d62b5aa075dd196eb716c69853
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-07-21 13:12:20 +0530

    Time: 529 ms (63.52%) | Memory: 0B (100.00%) - LeetSync

--- 1877-find-followers-count/find-followers-count.sql
+++ 1877-find-followers-count/find-followers-count.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select f1.user_id as user_id ,count(f1.follower_id) as followers_count from Followers f1   group by user_id order by f1.user_id asc;
+select user_id ,count(follower_id) as followers_count from Followers   group by user_id order by user_id asc;
