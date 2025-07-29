f94731f85742e14fd9d9f7bc457964f63393e154
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2025-07-29 16:33:49 +0530

    Time: 3 ms (82.93%) | Memory: 49.7 MB (53.77%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,32 @@
+class Solution {
+    private static int findOutIslands(char[][]grid,int ind,int jind)
+    {
+        if(ind<0||jind<0||ind==grid.length||jind==grid[0].length||grid[ind][jind]=='0')
+        {
+            return 0;
+        }
+        grid[ind][jind]='0';
+        int left=findOutIslands(grid,ind,jind-1);
+        int right = findOutIslands(grid,ind,jind+1);
+        int top = findOutIslands(grid,ind-1,jind);
+        int bottom = findOutIslands(grid,ind+1,jind);
+        return 1+left+right+top+bottom;
+    }
+    public int numIslands(char[][] grid) {
+        int cnt=0;
+        for(int ind =0;ind<grid.length;ind++)
+        {
+            for(int jind =0;jind<grid[ind].length;jind+=1)
+            {
+                if(grid[ind][jind]=='1')
+                {
+                    cnt++;
+                    findOutIslands(grid,ind,jind);
+                }
+            }
+        }
+        return cnt;
+     
+        
+    }
+}
