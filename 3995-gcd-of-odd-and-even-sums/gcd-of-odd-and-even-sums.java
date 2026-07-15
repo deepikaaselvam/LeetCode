39b7fa127cf979e3308746e5887e27cd13f8722a
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2026-07-15 21:34:50 +0530

    Time: 14 ms (12.29%) | Memory: 42.9 MB (6.12%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        int evenCount =0;
+        int evenCountSum =0;
+        int oddCount =-1;
+        int oddCountSum =0;
+        for(int ind =0;ind<n;ind++){
+            oddCount +=2;
+            oddCountSum+=oddCount;
+            evenCount+=2;
+            evenCountSum += evenCount;
+        }
+        System.out.println(oddCountSum +" "+ evenCountSum);
+        int largest =0;int smallest =0;
+         if(oddCountSum>evenCountSum){
+          largest = oddCountSum;
+          smallest = evenCountSum;
+         }else{
+            largest = evenCountSum;
+            smallest = oddCountSum;
+         }
+         if(smallest ==0){
+            return largest;
+         }
+         int remainder = 0;
+         while(smallest!= 0){
+            remainder = largest%smallest;
+            System.out.println("Remainder : "+remainder);
+            largest = smallest;
+            smallest = remainder;   
+         }
+         return largest;
+    }
+  
+}
